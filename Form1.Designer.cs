584786ef19440cca298e23e46cf7d660c4ed325c
Tin Truong Thanh <tintruong123@gmail.com>
2017-09-17 10:39:55 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace NQueens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lableTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lableTitle
+            // 
+            this.lableTitle.AutoSize = true;
+            this.lableTitle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lableTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lableTitle.ForeColor = System.Drawing.Color.Chocolate;
+            this.lableTitle.Location = new System.Drawing.Point(106, 28);
+            this.lableTitle.Name = "lableTitle";
+            this.lableTitle.Size = new System.Drawing.Size(340, 31);
+            this.lableTitle.TabIndex = 0;
+            this.lableTitle.Text = "BÀI TOÁN N-QUÂN HẬU";
+            this.lableTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(183, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 61);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.Location = new System.Drawing.Point(243, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 61);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(303, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 61);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Location = new System.Drawing.Point(183, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 61);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(243, 217);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 61);
+            this.button5.TabIndex = 1;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gray;
+            this.button6.Location = new System.Drawing.Point(303, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 61);
+            this.button6.TabIndex = 1;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(183, 275);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 61);
+            this.button7.TabIndex = 1;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gray;
+            this.button8.Location = new System.Drawing.Point(243, 275);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 61);
+            this.button8.TabIndex = 1;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(303, 275);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 61);
+            this.button9.TabIndex = 1;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 520);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lableTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lableTitle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
